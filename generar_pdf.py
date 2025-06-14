d33d334e736b57b6860c19e54c0f00d4840c1a2f
cybernovala <bartese823@gmail.com>
2025-06-14 11:29:59 -0400

    Update generar_pdf.py

--- generar_pdf.py
+++ generar_pdf.py
@@ -6,16 +6,12 @@ from datetime import datetime
 def generar_pdf(data):
     pdf = FPDF()
     pdf.add_page()
-    pdf.set_font("Arial", "B", 14)
-    
-    # Título en negrita y centrado
+    pdf.set_font("Arial", "B", 16)
     pdf.cell(0, 10, "CONTRATO DE TRABAJO", ln=True, align="C")
     pdf.ln(10)
 
-    # Cambiamos a fuente normal para el resto del texto
     pdf.set_font("Arial", "", 12)
 
-    # Datos con valores por defecto y convertidos a mayúsculas
     ciudad = data.get("ciudad", "Los Ángeles").upper()
     fecha = data.get("fecha", datetime.today().strftime("%d-%m-%Y"))
     empleador = data.get("empleador", "Juan Pérez Soto").upper()
@@ -23,7 +19,7 @@ def generar_pdf(data):
     dom_empleador = data.get("domicilio_empleador", "FICTICIO N°1").upper()
     comuna_empleador = data.get("comuna_empleador", "Los Ángeles").upper()
     trabajador = data.get("trabajador", "Juan Pérez Pérez").upper()
-    nacionalidad = data.get("nacionalidad_trabajador", "Chileno").upper()
+    nacionalidad = data.get("nacionalidad_trabajador", "ChILENO").upper()
     dom_trabajador = data.get("domicilio_trabajador", "FICTICIO N°1").upper()
     comuna_trabajador = data.get("comuna_trabajador", "Los Ángeles").upper()
     rut_trabajador = data.get("rut_trabajador", "99999999-9")
@@ -33,7 +29,6 @@ def generar_pdf(data):
     sueldo = data.get("sueldo_bruto", "SUELDO BASE MÁS BONOS, COMISIONES, HORAS EXTRAS Y OTROS").upper()
     inicio = data.get("fecha_inicio", datetime.today().strftime("%d-%m-%Y"))
 
-    # Texto del contrato en MAYÚSCULAS
     texto = f"""
 EN {ciudad}, A {fecha}, ENTRE {empleador}, RUT {rut_empleador}, CON DOMICILIO EN {dom_empleador}, COMUNA DE {comuna_empleador}, EN ADELANTE "EL EMPLEADOR", Y DON {trabajador}, DE NACIONALIDAD {nacionalidad}, RUT {rut_trabajador}, CON DOMICILIO EN {dom_trabajador}, COMUNA DE {comuna_trabajador}, EN ADELANTE "EL TRABAJADOR", SE HA CONVENIDO EL SIGUIENTE CONTRATO:
 
@@ -47,15 +42,13 @@ FIRMAS:
 
 __________________________                    __________________________
 {empleador}                                       {trabajador}
-(RUT: {rut_empleador})                                          (RUT: {rut_trabajador})
+(RUT: {rut_empleador})                           (RUT: {rut_trabajador})
 """
     pdf.multi_cell(0, 10, texto.strip())
 
-    # Exportar como string y codificar bien
     pdf_output = pdf.output(dest='S').encode('latin1')
     temp_stream = io.BytesIO(pdf_output)
 
-    # Agregar protección
     reader = PdfReader(temp_stream)
     writer = PdfWriter()
     writer.append_pages_from_reader(reader)
